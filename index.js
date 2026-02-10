37ac4f5fadcb739c49399b805a18bf66089cfd21
Danilo Martinho <136544824+T-Rexz2023@users.noreply.github.com>
2026-02-10 00:45:46 +0000

    Atualiza personagens com novos atributos e corrige mensagens de resposta

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('Hello World')
 })
 
 app.get('/oi', (req, res) => {
-  res.send('Olá mundo!')
+  res.send('Olá, mundo!')
 })
 
-const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+const lista = [
+  {
+    nome: 'Rick Sanchez',
+    imagem: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  },
+  {
+    nome: 'Morty Smith',
+    imagem: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  },
+  {
+    nome: 'Summer Smith',
+    imagem: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+]
 
 app.get('/personagens', (req, res) => {
   res.send(lista)
@@ -18,14 +31,20 @@ app.get('/personagens', (req, res) => {
 
 app.get('/personagens/:id', (req, res) => {
   const id = req.params.id
+
   const personagem = lista[id - 1]
+
   res.send(personagem)
 })
 
+// Informo ao Express que o Body da Requisição está em JSON
 app.use(express.json())
 
 app.post('/personagens', (req, res) => {
-  const novoPersonagem = req.body.nome
+  const novoPersonagem = {
+    nome: req.body.nome,
+    imagem: req.body.imagem
+  }
 
   lista.push(novoPersonagem)
 
@@ -35,19 +54,24 @@ app.post('/personagens', (req, res) => {
 app.put('/personagens/:id', (req, res) => {
   const id = req.params.id
   const nomeAtualizado = req.body.nome
+  const imagemAtualizada = req.body.imagem
 
-  lista[id - 1] = nomeAtualizado
+  lista[id - 1] = {
+    nome: nomeAtualizado,
+    imagem: imagemAtualizada
+  }
 
   res.send("Personagem atualizado com sucesso!")
 })
 
 app.delete('/personagens/:id', (req, res) => {
-  const ide = req.params.id
-  
-  lista.splice(ide - 1, 1)
+  const id = req.params.id
 
-  res.send("Personagem deletado com sucesso!")
+  lista.splice(id - 1, 1)
+
+  res.send("Personagem removido com sucesso!")
 })
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
