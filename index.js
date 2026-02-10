93949fba5e08fbd299711c399322f6c0e5fcf487
Danilo Martinho <136544824+T-Rexz2023@users.noreply.github.com>
2026-02-10 00:05:43 +0000

    Adiciona rota /personagens para listar personagens

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ app.get('/oi', (req, res) => {
   res.send('Olá mundo!')
 })
 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+
+app.get('/personagens', (req, res) => {
+  res.send(lista)
+})
+
+app.get('/personagens/:id', (req, res) => {
+  const id = req.params.id
+  const personagem = lista[id - 1]
+  res.send(personagem)
+})
+
+app.use(express.json())
+
+app.post('/personagens', (req, res) => {
+  const novoPersonagem = req.body.nome
+
+  res.send(novoPersonagem)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
